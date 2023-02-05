206e063fd54b3f77d207d51a7c589e774e916fce
Joe <admin@kuew.net>
2023-02-05 20:46:03 +0800

    更新文件夹名称

---
+++ src/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MHS;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(new Views.HomeView());
+
+    }
+   
+}
+
---
+++ src/Views/HomeView.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MHS.Views;
+
+public partial class HomeView : ContentPage
+{
+	public HomeView()
+	{
+		InitializeComponent();
+        NavigationPage.SetHasNavigationBar(this, false);
+
+    }
+
+    private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+    {
+
+    }
+
+    private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+    {
+
+    }
+}
